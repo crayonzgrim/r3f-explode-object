5622ac0cbeaab123e1d0e294c4332350dda9fb05
crayonzgrim <crayonzgrim@gmail.com>
2023-07-29 15:22:18 +0900

    :hammer: Add bluebird by myself

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,12 +2,13 @@ import { OrbitControls, Environment, Float } from "@react-three/drei";
 import { Heart } from "./Heart";
 import { Earth } from "./Earth";
 import { useControls } from "leva";
+import { BlueBird } from "./BlueBird";
 
 export const Experience = () => {
   const { item } = useControls({
     item: {
       value: "heart",
-      options: ["heart", "earth"],
+      options: ["heart", "earth", "bluebird"],
     },
   });
   return (
@@ -16,6 +17,7 @@ export const Experience = () => {
       <Float floatIntensity={2} speed={3}>
         <Heart scale={6} visible={item === "heart"} />
         <Earth scale={0.1} visible={item === "earth"} />
+        <BlueBird scale={6} visible={item === "bluebird"} />
       </Float>
       <Environment preset="sunset" background blur={0.4} />
     </>
